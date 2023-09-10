4f91ef7e5829e5238b2e75c7f395f064deabac88
Faaiz <faaiz.shah@accionlabs.com>
2023-09-10 15:54:31 +0530

    Accessibility in Next.js

--- app/component/Header/header.js
+++ app/component/Header/header.js
@@ -1,15 +1,48 @@
+'use client'
+
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function Header() {
+  const items = [
+    {
+      id: 1,
+      title: "Home",
+      href: "/",
+      activeSegment: null,
+    },
+    {
+      id: 2,
+      title: "About",
+      href: "/about",
+      activeSegment: "about",
+    },
+    {
+      id: 3,
+      title: "Contact",
+      href: "/contact",
+      activeSegment: "contact",
+    },
+  ];
+
+  // Inside you client component
+  const activeSegment = useSelectedLayoutSegment();
+
   return (
     <header className="header">
-      <a href="#default" class="logo">
+      <a href="#" class="logo">
         CompanyLogo
       </a>
       <div class="header-right">
-        <Link href="/">Home</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/about">About</Link>
+        {items.map((item) => (
+          <Link
+            key={item.id}
+            href={item.href}
+            className={activeSegment === item.activeSegment ? "active" : ""}
+          >
+            {item.title}
+          </Link>
+        ))}
       </div>
     </header>
   );
--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body className={inter.className}>
